c6974c54c788252e8f2c0abb013eb8348fce8e6b
diivanov <diivanov@itmo.ru>
2022-04-02 19:24:31 +0300

    обновление до реакта 18 версии

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
